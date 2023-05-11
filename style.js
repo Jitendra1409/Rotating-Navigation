dec4f29618e72714159de9d781b2300e50dfb75d
Jitendra <jitendragehlot86@gmail.com>
2023-05-11 22:27:54 +0530

    initial commit

---
+++ style.js
@@ -0,0 +1,6 @@
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelectorAll('.container')
+
+open.addEventListener('click', () => addEventListener.classList.add('show nav'))
+close.addEventListener('click', () => addEventListener.classList.add('show nav'))
